db10839a6b1efe1fa9d0fb44b855c735c1dcb06f
Siddharth Shyniben <siddharth.muscat@gmail.com>
2026-08-27 08:11:36 +0530

    Update scaffolding comments

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,5 +1,12 @@
 import { registry } from "../nodes/registry.js";
+/** @import { Node } from "../nodes/registry.js" */
 
+/**
+ * Recursive layout algorithm.
+ * @param {Node} node - The root of the tree to lay out
+ * @param {*} constraints - Available space to lay out within
+ * @todo Implement
+ */
 export function layout(node, constraints) {
 	throw new Error("Not implemented");
 }
--- src/paint/paint.js
+++ src/paint/paint.js
@@ -1,5 +1,12 @@
 import { registry } from "../nodes/registry.js";
+/** @import { Node } from "../nodes/registry.js" */
 
+/**
+ * Recursive paint walk.
+ * @param {Node} node - The root of the tree to paint
+ * @param {*} canvas - The canvas to paint into
+ * @todo Implement
+ */
 export function paint(node, canvas) {
 	throw new Error("Not implemented");
 }
