6d27b6f4999b07714214cbc2021619c42b2819af
Siddharth Shyniben <siddharth.muscat@gmail.com>
2026-08-30 09:50:39 +0530

    Implement text painting/measurement

--- src/nodes/text.js
+++ src/nodes/text.js
@@ -1,4 +1,6 @@
 import validate from "@nuff-said/validate";
+import wrap from "word-wrap";
+
 import { allNodeDefaultOpts, exactString, nodeOptionsSchema } from "../util/validate.js";
 
 /**
@@ -15,8 +17,8 @@ import { allNodeDefaultOpts, exactString, nodeOptionsSchema } from "../util/vali
  * A text node's options, along with
  * {@link import('../util/validate.js').NodeOptions}
  * @typedef {Object} TextOptions
- * @property {boolean} [wrap=false] - Whether to word-wrap the text to fit its width
- * @property {WrapOptions} [wrapOpts] - word-wrap options, used when `wrap` is true
+ * @property {boolean} [forceNoWrap=false] - Whether to force absolutely no wrapping
+ * @property {WrapOptions} [wrapOpts] - word-wrap options, used unless `forceNoWrap` is true
  */
 
 /**
@@ -51,7 +53,7 @@ export function text(text, options = {}) {
  * @type {TextOptions}
  */
 export const textDefaultOpts = {
-	wrap: false,
+	forceNoWrap: false,
 	wrapOpts: {},
 }
 
@@ -65,7 +67,7 @@ export const validateText = validate({
 	text: [validate.required(), validate.string()],
 	options: {
 		...nodeOptionsSchema,
-		wrap: validate.boolean(),
+		forceNoWrap: validate.boolean(),
 		wrapOpts: {
 			width: validate.number(),
 			indent: validate.number(),
@@ -83,7 +85,17 @@ export const validateText = validate({
  * @todo Implement
  */
 export function measureText(node, constraints) {
-	throw new Error("Not implemented");
+	const wrapOpts = { width: constraints.maxWidth }
+	const finalOpts = Object.assign({}, wrapOpts, node.options.wrapOpts);
+
+	const finalText = node.options.forceNoWrap
+		? node.text
+		: wrap(node.text, finalOpts);
+
+	return {
+		width: Math.max(...finalText.split(finalOpts.newline ?? "\n").map(l => l.length)),
+		height: finalText.split(finalOpts.newline ?? "\n").length
+	}
 }
 
 /**
@@ -94,5 +106,12 @@ export function measureText(node, constraints) {
  * @todo Implement
  */
 export function paintText(node, canvas, rect) {
-	throw new Error("Not implemented");
+	const wrapOpts = { width: rect.width }
+	const finalOpts = Object.assign({}, wrapOpts, node.options.wrapOpts);
+
+	const finalText = node.options.forceNoWrap
+		? node.text
+		: wrap(node.text, finalOpts);
+
+	finalText.split(finalOpts.newline ?? "\n").forEach((line, i) => canvas.moveTo(rect.x, rect.y + i).write(line))
 }
--- test/text.test.js
+++ test/text.test.js
@@ -6,14 +6,14 @@ test("text() applies node and text defaults", () => {
 	const node = text("hello");
 	assert.equal(node.type, "text");
 	assert.equal(node.text, "hello");
-	assert.equal(node.options.wrap, textDefaultOpts.wrap);
+	assert.equal(node.options.forceNoWrap, textDefaultOpts.forceNoWrap);
 	assert.deepEqual(node.options.wrapOpts, {});
 	assert.equal(node.options.flexShrink, 1);
 });
 
 test("text() merges wrapOpts instead of replacing them wholesale", () => {
-	const node = text("hello", { wrap: true, wrapOpts: { width: 10 } });
-	assert.equal(node.options.wrap, true);
+	const node = text("hello", { forceNoWrap: true, wrapOpts: { width: 10 } });
+	assert.equal(node.options.forceNoWrap, true);
 	assert.deepEqual(node.options.wrapOpts, { width: 10 });
 });
 
@@ -22,6 +22,6 @@ test("text() throws when text isn't a string", () => {
 });
 
 test("text() throws on invalid options", () => {
-	assert.throws(() => text("hello", { wrap: "yes" }), /wrap/);
+	assert.throws(() => text("hello", { forceNoWrap: "yes" }), /forceNoWrap/);
 });
 
